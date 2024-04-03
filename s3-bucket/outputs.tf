b20546cff3855f1bc8800d46ae229343fcdf3b50
Tjeerd Ritsma <tjeerd.ritsma@quinyx.com>
2024-04-03 19:08:41 +0200

    feat(s3-bucket): add OAC id to output

--- s3-bucket/outputs.tf
+++ s3-bucket/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_arn" {
 output "bucket_id" {
   value = module.s3-bucket.s3_bucket_id
 }
+
+output "cloudfront_oac_id" {
+  value = var.cloudfront_origin_access_control ? aws_cloudfront_origin_access_control.oac["access"].id : ""
+}
