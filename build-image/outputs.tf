332f1b881010afb9bdda19da7d5a2727e022f888
Tjeerd Ritsma <tjeerd.ritsma@quinyx.com>
2024-04-08 16:59:39 +0200

    fix(build-image): return tag reference as full image and use terraform output
    also display the metadata

--- build-image/outputs.tf
+++ build-image/outputs.tf
@@ -5,3 +5,10 @@ output "aws_ecr_repo_url" {
 output "aws_ecr_repo_arn" {
   value = aws_ecr_repository.repository[var.repository_name].arn
 }
+output "aws_ecr_cache_repo_url" {
+  value = var.cache ? aws_ecr_repository.repository[format("%s/cache", var.repository_name)].repository_url : ""
+}
+
+output "aws_ecr_cache_repo_arn" {
+  value = var.cache ? aws_ecr_repository.repository[format("%s/cache", var.repository_name)].arn : ""
+}
