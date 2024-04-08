aea9c15cd2795b857e3c20edbdd914a7e3a88881
Tjeerd Ritsma <tjeerd.ritsma@quinyx.com>
2024-04-08 17:31:26 +0200

    fix(build-image): small tweaks in repository name formatting

--- build-image/main.tf
+++ build-image/main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 locals {
   repos = merge({
-    var.repository_name = {
+    format("%s", var.repository_name) = {
       mutability = "IMMUTABLE"
       policy = templatefile(var.lifecycle_policy, {})
     } 
@@ -18,7 +18,7 @@ locals {
 
 # tfsec:ignore:aws-ecr-repository-customer-key
 resource "aws_ecr_repository" "repository" {
-  for_each = local.repos
+  for_each             = local.repos
   name                 = each.key
   image_tag_mutability = each.value.mutability
   encryption_configuration {
@@ -27,7 +27,7 @@ resource "aws_ecr_repository" "repository" {
 }
 
 resource "aws_ecr_lifecycle_policy" "policy" {
-  for_each = local.repos
+  for_each   = local.repos
   repository = aws_ecr_repository.repository[each.key].name
   policy     = each.value.policy
 }
--- build-image/outputs.tf
+++ build-image/outputs.tf
@@ -1,9 +1,9 @@
 output "aws_ecr_repo_url" {
-  value = aws_ecr_repository.repository[var.repository_name].repository_url
+  value = aws_ecr_repository.repository[format("%s", var.repository_name)].repository_url
 }
 
 output "aws_ecr_repo_arn" {
-  value = aws_ecr_repository.repository[var.repository_name].arn
+  value = aws_ecr_repository.repository[format("%s", var.repository_name)].arn
 }
 output "aws_ecr_cache_repo_url" {
   value = var.cache ? aws_ecr_repository.repository[format("%s/cache", var.repository_name)].repository_url : ""
